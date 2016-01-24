8ad6699ceb3c17dd1b3fad2b2fb6f1837fd122e4
Sean Perkins <perkinsean@gmail.com>
2016-01-24 16:26:04 -0500

    Added server time to footer to display the year

--- routes.py
+++ routes.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from operator import itemgetter
 import requests
+import datetime
 
 app = Flask(__name__)
 
@@ -12,22 +13,29 @@ def get_repos():
     repos = [x for x in repos if not x['fork'] and not x['private']]
     return repos
 
+@app.template_filter()
+def datetimefilter(value, format='%Y'):
+    return value.strftime(format)
+
+app.jinja_env.filters['datetimefilter'] = datetimefilter
+
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', current_time = datetime.datetime.now())
 
 @app.route('/projects')
 def projects():
     repos = get_repos()
-    return render_template('projects.html', repos=repos)
+    return render_template('projects.html', repos=repos, current_time =
+            datetime.datetime.now())
 
 @app.route('/resume')
 def resume():
-    return render_template('resume.html')
+    return render_template('resume.html', current_time = datetime.datetime.now())
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template('404.html'), 404
+    return render_template('404.html',current_time = datetime.datetime.now()), 404
 
 if __name__ == "__main__":
     app.run()
